3b49199929b2d846b5b9ae0d90c40abf4be5b9ca
孙献泳 <sunxianyong@jinse.com>
2021-08-31 13:31:34 +0800

    请求体

--- services/mssg.py
+++ services/mssg.py
@@ -1,12 +1,20 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
 from starlette.requests import Request
-from starlette.responses import HTMLResponse, Response
+from starlette.responses import HTMLResponse
+from wechatpy import parse_message
+from wechatpy.replies import TextReply
+
 
 router = APIRouter()
 
 
 @router.post("/weixin")
 async def mssg(request: Request):
-    print(await request.body())
-    return HTMLResponse('success')
+    msg = parse_message(await request.body())
+
+    reply = TextReply(message=msg)
+    reply.content = '测试'
+    # 转换成 XML
+    xml = reply.render()
+
+    return HTMLResponse(xml)
