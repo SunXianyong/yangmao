0896868f629e6bf2bc5057ac6d7d7ed0d0b11c26
孙献泳 <sunxianyong@jinse.com>
2021-09-04 18:12:56 +0800

    test

--- services/mssg.py
+++ services/mssg.py
@@ -4,6 +4,7 @@ from starlette.responses import HTMLResponse
 from wechatpy import parse_message
 from wechatpy.replies import TextReply, to_text
 from wechatpy.fields import BaseField
+from wechatpy.messages import TextMessage
 
 router = APIRouter()
 
@@ -40,13 +41,32 @@ async def mssg(request: Request):
     msg = parse_message(await request.body())
     print(msg)
 
+    if msg.get('Content') == 'test':
+        return test(msg)
+
     reply = NewTextReply(message=msg)
-    reply.content = 'Hi，你的每天红包已供上，请先收下！' \
+    reply.content = 'Hi，你的每天红包已供上，请先收下！\n' \
                     '🧧&lt;a href=&quot;http://dpurl.cn/7DnghG2z&quot;&gt;美团外卖-每日红包&lt;/a&gt;\n' \
                     '🧧&lt;a href=&quot;http://y6.pub/uH8CWP&quot;&gt;美团生鲜超市-每日红包&lt;/a&gt;'
+    # '--&lt;a href=&quot;weixin://bizmsgmenu?msgmenucontent=帮助&amp;msgmenuid=0&quot;&gt;【有什么不懂可以点我】&lt;/a&gt;--'
     # reply.content = '🧧&lt;a href=&quot;weixin://bizmsgmenu?msgmenucontent=美团红包&amp;msgmenuid=0&quot;&gt;美团红包&lt;/a&gt;\n' \
     #                 '🧧&lt;a href=&quot;weixin://bizmsgmenu?msgmenucontent=饿了吗红包&amp;msgmenuid=0&quot;&gt;饿了吗红包&lt;/a&gt;'
 
     # 转换成 XML
     xml = reply.render()
     return HTMLResponse(xml)
+
+
+async def test(msg: TextMessage) -> HTMLResponse:
+    reply = NewTextReply(message=msg)
+    # reply.content = 'Hi，你的每天红包已供上，请先收下！\n' \
+    #                 '🧧&lt;a href=&quot;http://dpurl.cn/7DnghG2z&quot;&gt;美团外卖-每日红包&lt;/a&gt;\n' \
+    #                 '🧧&lt;a href=&quot;http://y6.pub/uH8CWP&quot;&gt;美团生鲜超市-每日红包&lt;/a&gt;\n' \
+    #                 '--&lt;a href=&quot;weixin://bizmsgmenu?msgmenucontent=帮助&amp;msgmenuid=0&quot;&gt;' \
+    #                 '【有什么不懂可以点我】&lt;/a&gt;--'
+    reply.content = """Hi，你的每天红包已供上，请先收下！
+    🧧&lt;a href=&quot;http://dpurl.cn/7DnghG2z&quot;&gt;美团外卖-每日红包&lt;/a&gt;\n
+    🧧&lt;a href=&quot;http://y6.pub/uH8CWP&quot;&gt;美团生鲜超市-每日红包&lt;/a&gt;\n
+    --&lt;a href=&quot;weixin://bizmsgmenu?msgmenucontent=帮助&amp;msgmenuid=0&quot;&gt;【有什么不懂可以点我】&lt;/a&gt;--
+    """
+    return HTMLResponse(reply.render())
