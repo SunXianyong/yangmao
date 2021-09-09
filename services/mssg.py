65d6a416ce8faa9c1e97743009ac9d5cfea6878a
孙献泳 <sunxianyong@jinse.com>
2021-09-09 12:13:27 +0800

    eleme

--- services/mssg.py
+++ services/mssg.py
@@ -5,6 +5,7 @@ from wechatpy import parse_message
 from wechatpy.replies import TextReply, to_text
 from wechatpy.fields import BaseField
 from wechatpy.messages import TextMessage
+from init_wecahtclient import client
 
 router = APIRouter()
 
@@ -44,8 +45,8 @@ async def mssg(request: Request):
 
     if content == 'test':
         return await test(msg)
-    elif content == '帮助':
-        return
+    elif content == '饿了么':
+        return await eleme(msg)
 
     reply = NewTextReply(message=msg)
     reply.content = """Hi，每天红包已供上，请先收下！
@@ -63,12 +64,32 @@ async def mssg(request: Request):
     return HTMLResponse(xml)
 
 
+async def eleme(msg: TextMessage) -> HTMLResponse:
+    reply = NewTextReply(message=msg)
+    # access = client.access_token
+    img_list = client.material.batchget('image')
+    print(img_list)
+
+    reply.content = """Hi，每天红包已供上，请先收下！
+    🧧&lt;a href=&quot;http://dpurl.cn/7DnghG2z&quot;&gt;美团外卖-每日红包①&lt;/a&gt;
+    🧧&lt;a href=&quot;http://dpurl.cn/9joHoGaz&quot;&gt;美团外卖-每日红包②&lt;/a&gt;
+    🧧&lt;a href=&quot;http://y6.pub/uH8CWP&quot;&gt;美团生鲜超市-每日红包&lt;/a&gt;
+    🧧&lt;a href=&quot;weixin://bizmsgmenu?msgmenucontent=饿了么红包&amp;msgmenuid=0&quot;&gt;饿了么&lt;/a&gt;
+    小技巧：
+    红包金额随机，多领一个用大的。
+    """
+    # --&lt;a href=&quot;weixin://bizmsgmenu?msgmenucontent=帮助&amp;msgmenuid=0&quot;&gt;【有什么不懂可以点我】&lt;/a&gt;--
+    # """
+    return HTMLResponse(reply.render())
+
+
 async def test(msg: TextMessage) -> HTMLResponse:
     reply = NewTextReply(message=msg)
     reply.content = """Hi，每天红包已供上，请先收下！
     🧧&lt;a href=&quot;http://dpurl.cn/7DnghG2z&quot;&gt;美团外卖-每日红包①&lt;/a&gt;
     🧧&lt;a href=&quot;http://dpurl.cn/9joHoGaz&quot;&gt;美团外卖-每日红包②&lt;/a&gt;
     🧧&lt;a href=&quot;http://y6.pub/uH8CWP&quot;&gt;美团生鲜超市-每日红包&lt;/a&gt;
+    🧧&lt;a href=&quot;weixin://bizmsgmenu?msgmenucontent=饿了么红包&amp;msgmenuid=0&quot;&gt;饿了么&lt;/a&gt;
     小技巧：
     红包金额随机，多领一个用大的。
     """
